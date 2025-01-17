68f3e3d7068fca35a6137ef7d5968ddf4e9ef3e7
favstats <fabio.votta@gmail.com>
2025-01-17 09:49:39 +0100

    update

--- retrieve_targeting_data.R
+++ retrieve_targeting_data.R
@@ -13,23 +13,45 @@ library(httr2)
 thecntry <- "DE"
 
 
-Sys.setenv(LIBARROW_MINIMAL = "false")
-Sys.setenv("NOT_CRAN" = "true")
+thepkgs <-  installed.packages() %>% as_tibble() %>% pull(Package)
 
-print("##### please install arrow #####")
 
-options(
-  HTTPUserAgent =
-    sprintf(
-      "R/%s R (%s)",
-      getRversion(),
-      paste(getRversion(), R.version["platform"], R.version["arch"], R.version["os"])
+if(!("arrow" %in% thepkgs)){
+  
+  if (!(Sys.info()[["effective_user"]] %in% c("fabio", "favstats"))) {
+    remove.packages("arrow")
+  }
+  
+  Sys.setenv(LIBARROW_MINIMAL = "false")
+  Sys.setenv("NOT_CRAN" = "true")
+  
+  print("##### please install arrow #####")
+  
+  options(
+    HTTPUserAgent =
+      sprintf(
+        "R/%s R (%s)",
+        getRversion(),
+        paste(getRversion(), R.version["platform"], R.version["arch"], R.version["os"])
+      )
+  )
+  if (!(Sys.info()[["effective_user"]] %in% c("fabio", "favstats"))) {
+    suppressMessages(
+      suppressWarnings(
+        install.packages(
+          "arrow",
+          repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest",
+          quiet = TRUE
+        )
+      )
     )
-)
-if (!(Sys.info()[["effective_user"]] %in% c("fabio", "favstats"))) {
-  install.packages("arrow", repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest")
-  arrow::install_arrow(verbose = F) # verbose output to debug install errors
+    arrow::install_arrow(verbose = F) # verbose output to debug install errors
+  }
+  
 }
+
+
+
 
 
 # ips_targeting <- read_lines("ips-targeting.txt")
